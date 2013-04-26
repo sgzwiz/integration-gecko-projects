834e31c69ae7c857f69a069273f28f2d0b55c0af
Jonathan Griffin <jgriffin@mozilla.com>
2013-04-26 15:16:57 -0700

    Restore original timeout

--- testing/marionette/client/marionette/emulator.py
+++ testing/marionette/client/marionette/emulator.py
@@ -284,7 +284,7 @@ waitFor(
         now = datetime.datetime.now()
         while online == set([]):
             time.sleep(1)
-            if datetime.datetime.now() - now > datetime.timedelta(seconds=180):
+            if datetime.datetime.now() - now > datetime.timedelta(seconds=60):
                 raise Exception('timed out waiting for emulator to be available')
             online, offline = self._get_adb_devices()
         self.port = int(list(online)[0])
@@ -320,7 +320,7 @@ waitFor(
         now = datetime.datetime.now()
         while online - original_online == set([]):
             time.sleep(1)
-            if datetime.datetime.now() - now > datetime.timedelta(seconds=180):
+            if datetime.datetime.now() - now > datetime.timedelta(seconds=60):
                 raise Exception('timed out waiting for emulator to start')
             online, offline = self._get_adb_devices()
         self.port = int(list(online - original_online)[0])
