84783bd61892c91f408b51aa60653dd5cd271499
Jonathan Griffin <jgriffin@mozilla.com>
2013-04-26 11:51:46 -0700

    Bug Bug 859867 - More debug output

--- testing/marionette/client/marionette/emulator.py
+++ testing/marionette/client/marionette/emulator.py
@@ -231,6 +231,7 @@ class Emulator(object):
         offline = set()
         online = set()
         output = self._run_adb(['devices'])
+        print 'get_adb_devices: ', output
         for line in output.split('\n'):
             m = self.deviceRe.match(line)
             if m:
@@ -284,7 +285,7 @@ waitFor(
         now = datetime.datetime.now()
         while online == set([]):
             time.sleep(1)
-            if datetime.datetime.now() - now > datetime.timedelta(seconds=180):
+            if datetime.datetime.now() - now > datetime.timedelta(seconds=60):
                 raise Exception('timed out waiting for emulator to be available')
             online, offline = self._get_adb_devices()
         self.port = int(list(online)[0])
@@ -313,14 +314,13 @@ waitFor(
         original_online, original_offline = self._get_adb_devices()
 
         self.proc = subprocess.Popen(qemu_args,
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE)
+                                     stderr=subprocess.STDOUT)
 
         online, offline = self._get_adb_devices()
         now = datetime.datetime.now()
         while online - original_online == set([]):
             time.sleep(1)
-            if datetime.datetime.now() - now > datetime.timedelta(seconds=180):
+            if datetime.datetime.now() - now > datetime.timedelta(seconds=60):
                 raise Exception('timed out waiting for emulator to start')
             online, offline = self._get_adb_devices()
         self.port = int(list(online - original_online)[0])
