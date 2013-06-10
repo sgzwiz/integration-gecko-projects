ff51c94def37948269710561ac4807bc5c5fdbca
Michal Novotny <michal.novotny@gmail.com>
2013-06-11 01:29:49 +0200

    CACHE2: CacheFile implementation

---
+++ netwerk/cache2/CacheFileUtils.h
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef CacheFileUtils__h__
+#define CacheFileUtils__h__
+
+#include "CacheFileChunk.h"
+#include "nsIRunnable.h"
+#include "nsThreadUtils.h"
+#include "nsCOMPtr.h"
+#include "nsAutoPtr.h"
+
+namespace mozilla {
+namespace net {
+
+class CacheFile;
+
+class GapFiller : public nsRunnable
+                , public CacheFileChunkListener
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIRUNNABLE
+
+  GapFiller(uint32_t aStartChunk, uint32_t aEndChunk,
+            CacheFileChunkListener *aCB, CacheFile *aFile);
+  ~GapFiller();
+
+  nsresult OnChunkRead(nsresult aResult, CacheFileChunk *aChunk);
+  nsresult OnChunkWritten(nsresult aResult, CacheFileChunk *aChunk);
+  nsresult OnChunkAvailable(nsresult aResult, CacheFileChunk *aChunk);
+  nsresult OnChunkUpdated(CacheFileChunk *aChunk);
+
+protected:
+  nsresult NotifyListener(nsresult aResult, CacheFileChunk *aChunk);
+
+  uint32_t                         mStartChunk;
+  uint32_t                         mEndChunk;
+  nsCOMPtr<CacheFileChunkListener> mListener;
+  nsRefPtr<CacheFile>              mFile;
+};
+
+} // net
+} // mozilla
+
+#endif
