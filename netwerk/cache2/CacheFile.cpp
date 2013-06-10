33fe35d8ecb0f4e6f099c8ab9d6ea33811aa3249
Michal Novotny <michal.novotny@gmail.com>
2013-06-11 01:29:49 +0200

    CACHE2: CacheFile implementation

---
+++ netwerk/cache2/CacheFile.cpp
@@ -0,0 +1,607 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "CacheFile.h"
+
+#include "CacheFileChunk.h"
+#include "CacheFileInputStream.h"
+#include "CacheFileOutputStream.h"
+#include "CacheFileUtils.h"
+#include "nsThreadUtils.h"
+#include <algorithm>
+
+namespace mozilla {
+namespace net {
+
+class NotifyChunkListenerEvent : public nsRunnable {
+public:
+  NotifyChunkListenerEvent(CacheFileChunkListener *aCallback,
+                           nsresult aResult,
+                           CacheFileChunk *aChunk)
+    : mCallback(aCallback)
+    , mRV(aResult)
+    , mChunk(aChunk)
+  {
+    MOZ_COUNT_CTOR(NotifyChunkListenerEvent);
+  }
+
+  ~NotifyChunkListenerEvent()
+  {
+    MOZ_COUNT_DTOR(NotifyChunkListenerEvent);
+  }
+
+  NS_IMETHOD Run()
+  {
+    mCallback->OnChunkAvailable(mRV, mChunk);
+    return NS_OK;
+  }
+
+protected:
+  nsCOMPtr<CacheFileChunkListener> mCallback;
+  nsresult                         mRV;
+  nsRefPtr<CacheFileChunk>         mChunk;
+};
+
+NS_IMPL_THREADSAFE_ISUPPORTS3(CacheFile,
+                              CacheFileChunkListener,
+                              CacheFileIOListener,
+                              CacheFileMetadataListener)
+
+CacheFile::CacheFile()
+  : mLock("CacheFile.mLock")
+  , mReady(false)
+  , mMemoryOnly(false)
+  , mDataAccessed(false)
+  , mDataSize(-1)
+{
+  MOZ_COUNT_CTOR(CacheFile);
+  mChunks.Init();
+  mChunkListeners.Init();
+}
+
+CacheFile::~CacheFile()
+{
+  MOZ_COUNT_DTOR(CacheFile);
+}
+
+nsresult
+CacheFile::Init(const nsACString &aKey,
+                bool aCreateNew,
+                CacheFileListener *aCallback)
+{
+  MOZ_ASSERT(!mListener);
+  MOZ_ASSERT(!mHandle);
+
+  nsresult rv;
+
+  mKey = aKey;
+
+  SHA1Sum sum;
+  SHA1Sum::Hash hash;
+  sum.update(mKey.get(), mKey.Length());
+  sum.finish(hash);
+
+  uint32_t flags;
+  if (aCreateNew)
+    flags = CacheFileIOManager::CREATE_NEW;
+  else
+    flags = CacheFileIOManager::CREATE;
+
+  mListener = aCallback;
+  rv = CacheFileIOManager::OpenFile(&hash, flags, this);
+  if (NS_FAILED(rv)) {
+    mListener = nullptr;
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::OnChunkRead(nsresult aResult, CacheFileChunk *aChunk)
+{
+  MutexAutoLock lock(mLock);
+
+  uint32_t index = aChunk->Index();
+
+  if (NS_SUCCEEDED(aResult)) {
+    MOZ_ASSERT(aChunk->DataSize() != 0);
+    // check that hash matches
+    if (aChunk->Hash() != mMetadata->GetHash(index)) {
+      aResult = NS_ERROR_FILE_CORRUPTED;
+      aChunk->mRemovingChunk = true;
+      mChunks.Remove(index);
+      aChunk = nullptr;
+      // TODO notify other streams ???
+    }
+  }
+
+  if (NS_SUCCEEDED(aResult))
+    aChunk->SetReady(true);
+
+  return NotifyChunkListeners(index, aResult, aChunk);
+}
+
+nsresult
+CacheFile::OnChunkWritten(nsresult aResult, CacheFileChunk *aChunk)
+{
+  MutexAutoLock lock(mLock);
+
+  nsresult rv;
+
+  if (NS_FAILED(aResult)) {
+    // TODO ??? doom entry
+    NotifyChunkListeners(aChunk->Index(), aResult, nullptr);
+  }
+  else {
+    // update hash value in metadata
+    mMetadata->SetHash(aChunk->Index(), aChunk->Hash());
+
+    // notify listeners if there is any
+    ChunkListeners *listeners;
+    mChunkListeners.Get(aChunk->Index(), &listeners);
+    if (listeners) {
+      // don't release the chunk since there are some listeners queued
+      rv = NotifyChunkListeners(aChunk->Index(), NS_OK, aChunk);
+      if (NS_SUCCEEDED(rv)) {
+        MOZ_ASSERT(aChunk->mRefCnt != 2);
+        aChunk->SetReady(true);
+        return NS_OK;
+      }
+
+      NS_WARNING("NotifyChunkListeners failed???");
+      if (aChunk->mRefCnt != 2) {
+        // Some of the listeners didn't fail and got the reference
+        aChunk->SetReady(true);
+        return NS_OK;
+      }
+    }
+  }
+
+  MOZ_ASSERT(aChunk->mRefCnt == 2);
+  aChunk->mRemovingChunk = true;
+  mChunks.Remove(aChunk->Index());
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::OnChunkAvailable(nsresult aResult, CacheFileChunk *aChunk)
+{
+  MOZ_NOT_REACHED("CacheFile::OnChunkAvailable should not be called!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+nsresult
+CacheFile::OnChunkUpdated(CacheFileChunk *aChunk)
+{
+  MOZ_NOT_REACHED("CacheFile::OnChunkUpdated should not be called!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+nsresult
+CacheFile::OnFileOpened(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_ASSERT(mListener);
+
+  nsresult rv;
+  nsCOMPtr<CacheFileListener> listener;
+
+  if (NS_FAILED(aResult)) {
+    mListener.swap(listener);
+    listener->OnFileReady(aResult);
+    return NS_OK;
+  }
+
+  mHandle = aHandle;
+
+  mMetadata = new CacheFileMetadata(mHandle, mKey);
+
+  rv = mMetadata->ReadMetadata(this);
+  if (NS_FAILED(rv)) {
+    mListener.swap(listener);
+    listener->OnFileReady(rv);
+    return NS_OK;
+  }
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::OnDataWritten(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_NOT_REACHED("CacheFile::OnDataWritten should not be called!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+nsresult
+CacheFile::OnDataRead(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_NOT_REACHED("CacheFile::OnDataRead should not be called!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+nsresult
+CacheFile::OnMetadataRead(nsresult aResult)
+{
+  MOZ_ASSERT(mListener);
+
+  if (NS_SUCCEEDED(aResult)) {
+    mReady = true;
+    mDataSize = mMetadata->Offset();
+  }
+
+  nsCOMPtr<CacheFileListener> listener;
+  mListener.swap(listener);
+  listener->OnFileReady(aResult);
+  return NS_OK;
+}
+
+nsresult
+CacheFile::OnMetadataWritten(nsresult aResult)
+{
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+nsresult
+CacheFile::OnFileDoomed(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_ASSERT(mListener);
+
+  nsCOMPtr<CacheFileListener> listener;
+  mListener.swap(listener);
+  listener->OnFileDoomed(aResult);
+  return NS_OK;
+}
+
+nsresult
+CacheFile::OpenInputStream(nsIInputStream **_retval)
+{
+  MutexAutoLock lock(mLock);
+
+  MOZ_ASSERT(mHandle);
+
+  if (!mReady)
+    return NS_ERROR_NOT_AVAILABLE;
+
+  CacheFileInputStream *input = new CacheFileInputStream(this);
+
+  mInputs.AppendElement(input);
+
+  mDataAccessed = true;
+  NS_ADDREF(*_retval = input);
+  return NS_OK;
+}
+
+nsresult
+CacheFile::OpenOutputStream(nsIOutputStream **_retval)
+{
+  MutexAutoLock lock(mLock);
+
+  MOZ_ASSERT(mHandle);
+
+  if (!mReady)
+    return NS_ERROR_NOT_AVAILABLE;
+
+  if (mOutput)
+    return NS_ERROR_NOT_AVAILABLE;
+
+  mOutput = new CacheFileOutputStream(this);
+
+  mDataAccessed = true;
+  NS_ADDREF(*_retval = mOutput);
+  return NS_OK;
+}
+
+nsresult
+CacheFile::SetMemoryOnly(bool aMemoryOnly)
+{
+  MOZ_ASSERT(mReady);
+  // MemoryOnly can be set only before we start reading/writting data
+  MOZ_ASSERT(!mDataAccessed);
+
+  if (!mReady || mDataAccessed)
+    return NS_ERROR_UNEXPECTED;
+
+  // TODO what to do when this isn't a new entry and has an existing metadata???
+  mMemoryOnly = aMemoryOnly;
+  return NS_OK;
+}
+
+nsresult
+CacheFile::Doom(CacheFileListener *aCallback)
+{
+  MOZ_ASSERT(!mListener);
+  MOZ_ASSERT(mHandle || mMemoryOnly);
+
+  nsresult rv;
+
+  if (mMemoryOnly) {
+    // TODO what exactly to do here?
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+  else {
+    mListener = aCallback;
+    rv = CacheFileIOManager::DoomFile(mHandle, aCallback ? this : nullptr);
+    if (NS_FAILED(rv)) {
+      mListener = nullptr;
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+  }
+
+  return NS_OK;
+}
+
+mozilla::Mutex *
+CacheFile::GetLock()
+{
+  return &mLock;
+}
+
+void
+CacheFile::AssertOwnsLock()
+{
+  mLock.AssertCurrentThreadOwns();
+}
+
+nsresult
+CacheFile::GetChunk(uint32_t aIndex, bool aWriter,
+                    CacheFileChunkListener *aCallback)
+{
+  MutexAutoLock lock(mLock);
+  return GetChunkLocked(aIndex, aWriter, aCallback);
+}
+
+nsresult
+CacheFile::GetChunkLocked(uint32_t aIndex, bool aWriter,
+                          CacheFileChunkListener *aCallback)
+{
+  AssertOwnsLock();
+
+  MOZ_ASSERT(mReady);
+  MOZ_ASSERT(mHandle || mMemoryOnly);
+
+  nsresult rv;
+
+  nsRefPtr<CacheFileChunk> chunk;
+  if (mChunks.Get(aIndex, getter_AddRefs(chunk))) {
+    if (chunk->IsReady())
+      rv = NotifyChunkListener(aCallback, nullptr, NS_OK, chunk);
+    else
+      rv = QueueChunkListener(aIndex, aCallback);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    return NS_OK;
+  }
+
+  int64_t off = aIndex * kChunkSize;
+
+  if (off < mDataSize) {
+    // We cannot be here if this is memory only entry since the chunk must exist
+    MOZ_ASSERT(!mMemoryOnly);
+
+    rv = QueueChunkListener(aIndex, aCallback);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    chunk = new CacheFileChunk(this, aIndex);
+    mChunks.Put(aIndex, chunk);
+
+    // Read the chunk from the disk
+    rv = chunk->Read(mHandle, std::min(static_cast<uint32_t>(mDataSize - off),
+                     static_cast<uint32_t>(kChunkSize)), this);
+    if (NS_FAILED(rv)) {
+      chunk->mRemovingChunk = true;
+      mChunks.Remove(aIndex);
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+
+    return NS_OK;
+  }
+  else if (off == mDataSize) {
+    if (aWriter) {
+      // this listener is going to write to the chunk
+      rv = QueueChunkListener(aIndex, aCallback);
+      NS_ENSURE_SUCCESS(rv, rv);
+
+      chunk = new CacheFileChunk(this, aIndex);
+      mChunks.Put(aIndex, chunk);
+      chunk->InitNew(this);
+      chunk->SetReady(true);
+
+      rv = NotifyChunkListeners(aIndex, NS_OK, chunk);
+      NS_ENSURE_SUCCESS(rv, rv);
+
+      return NS_OK;
+    }
+  }
+  else {
+    if (aWriter) {
+      // this chunk was requested by writer, but we need to fill the gap first
+      if (mGapFiller) {
+        rv = NotifyChunkListener(aCallback, nullptr, NS_ERROR_NOT_AVAILABLE,
+                                 nullptr);
+        NS_ENSURE_SUCCESS(rv, rv);
+
+        return NS_OK;
+      }
+
+      mGapFiller = new GapFiller(off / kChunkSize, aIndex, aCallback, this);
+      NS_DispatchToCurrentThread(mGapFiller);
+
+      return NS_OK;
+    }
+  }
+
+  if (mOutput)
+    // the chunk doesn't exist but mOutput may create it
+    rv = QueueChunkListener(aIndex, aCallback);
+  else
+    // the chunk doesn't exist and nobody is going to create it
+    rv = NotifyChunkListener(aCallback, nullptr, NS_ERROR_NOT_AVAILABLE,
+                             nullptr);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::RemoveChunk(CacheFileChunk *aChunk)
+{
+  nsresult rv;
+
+  // Avoid lock reentrancy by increasing the RefCnt
+  nsRefPtr<CacheFileChunk> chunk = aChunk;
+
+  {
+    MutexAutoLock lock(mLock);
+
+    MOZ_ASSERT(mReady);
+    MOZ_ASSERT(mHandle || mMemoryOnly);
+
+    if (aChunk->mRefCnt != 2) {
+      // somebody got the reference before the lock was acquired
+      return NS_OK;
+    }
+
+    if (mMemoryOnly) {
+      // cannot release chunks in case of memory only entry
+      return NS_OK;
+    }
+
+#ifdef DEBUG
+    {
+      // We can be here iff the chunk is in the hash table
+      nsRefPtr<CacheFileChunk> chunkCheck;
+      mChunks.Get(chunk->Index(), getter_AddRefs(chunkCheck));
+      MOZ_ASSERT(chunkCheck == chunk);
+
+      // We also shouldn't have any queued listener for this chunk
+      ChunkListeners *listeners;
+      mChunkListeners.Get(chunk->Index(), &listeners);
+      MOZ_ASSERT(listeners);
+    }
+#endif
+
+    if (chunk->IsDirty()) {
+      aChunk->SetReady(false);
+      rv = chunk->Write(mHandle, this);
+      if (NS_FAILED(rv)) {
+        MOZ_ASSERT(false, "Unexpected failure while writting chunk");
+      }
+      else {
+        // Chunk will be removed in OnChunkWritten if it is still unused
+        return NS_OK;
+      }
+    }
+
+    chunk->mRemovingChunk = true;
+    mChunks.Remove(chunk->Index());
+  }
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::RemoveInput(CacheFileInputStream *aInput)
+{
+  MutexAutoLock lock(mLock);
+
+#ifdef DEBUG
+  bool found =
+#endif
+  mInputs.RemoveElement(aInput);
+  MOZ_ASSERT(found);
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::RemoveOutput(CacheFileOutputStream *aOutput)
+{
+  MutexAutoLock lock(mLock);
+
+  // TODO cancel all queued chunk listeners that cannot be satisfied
+
+  MOZ_ASSERT(mOutput == aOutput);
+
+  if (mOutput != aOutput)
+    return NS_ERROR_FAILURE;
+
+  mOutput = nullptr;
+  return NS_OK;
+}
+
+nsresult
+CacheFile::NotifyChunkListener(CacheFileChunkListener *aCallback,
+                               nsIEventTarget *aTarget,
+                               nsresult aResult,
+                               CacheFileChunk *aChunk)
+{
+  nsresult rv;
+  nsRefPtr<NotifyChunkListenerEvent> ev;
+  ev = new NotifyChunkListenerEvent(aCallback, aResult, aChunk);
+  if (aTarget)
+    rv = aTarget->Dispatch(ev, NS_DISPATCH_NORMAL);
+  else
+    rv = NS_DispatchToCurrentThread(ev);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  return NS_OK;
+}
+
+nsresult
+CacheFile::QueueChunkListener(uint32_t aIndex,
+                              CacheFileChunkListener *aCallback)
+{
+  AssertOwnsLock();
+
+  ChunkListenerItem *item = new ChunkListenerItem();
+  item->mTarget = NS_GetCurrentThread();
+  item->mCallback = aCallback;
+
+  ChunkListeners *listeners;
+  if (!mChunkListeners.Get(aIndex, &listeners)) {
+    listeners = new ChunkListeners();
+    mChunkListeners.Put(aIndex, listeners);
+  }
+
+  listeners->mItems.AppendElement(item);
+  return NS_OK;
+}
+
+nsresult
+CacheFile::NotifyChunkListeners(uint32_t aIndex, nsresult aResult,
+                                CacheFileChunk *aChunk)
+{
+  AssertOwnsLock();
+
+  nsresult rv, rv2;
+
+  ChunkListeners *listeners;
+  mChunkListeners.Get(aIndex, &listeners);
+  mChunkListeners.Remove(aIndex);
+  MOZ_ASSERT(listeners);
+
+  rv = NS_OK;
+  for (uint32_t i = 0 ; i < listeners->mItems.Length() ; i++) {
+    ChunkListenerItem *item = listeners->mItems[i];
+    rv2 = NotifyChunkListener(item->mCallback, item->mTarget, aResult, aChunk);
+    if (NS_FAILED(rv2) && NS_SUCCEEDED(rv))
+      rv = rv2;
+    delete item;
+  }
+  delete listeners;
+
+  return rv;
+}
+
+int64_t
+CacheFile::DataSize()
+{
+  MutexAutoLock lock(mLock);
+  return mDataSize;
+}
+
+} // net
+} // mozilla
---
+++ netwerk/cache2/CacheFileMetadata.cpp
@@ -0,0 +1,477 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "CacheFileMetadata.h"
+
+#include "CacheLog.h"
+#include "CacheFileIOManager.h"
+#include "CacheHashUtils.h"
+#include "CacheFileChunk.h"
+#include "../cache/nsCacheUtils.h"
+#include "mozilla/Telemetry.h"
+
+
+namespace mozilla {
+namespace net {
+
+#define kMinMetadataRead 1024  // TODO find optimal value from telemetry
+#define kAlignSize       4096
+
+NS_IMPL_THREADSAFE_ISUPPORTS0(CacheFileMetadata)
+
+CacheFileMetadata::CacheFileMetadata(CacheFileHandle *aHandle, const nsACString &aKey)
+  : mHandle(aHandle)
+  , mHashArray(nullptr)
+  , mHashArraySize(0)
+  , mOffset(-1)
+  , mBuf(nullptr)
+  , mBufSize(0)
+  , mWriteBuf(nullptr)
+{
+  MOZ_COUNT_CTOR(CacheFileMetadata);
+  memset(&mMetaHdr, 0, sizeof(CacheFileMetadataHeader));
+  mKey = aKey;
+}
+
+CacheFileMetadata::~CacheFileMetadata()
+{
+  MOZ_COUNT_DTOR(CacheFileMetadata);
+  MOZ_ASSERT(!mListener);
+
+  if (mHashArray) {
+    free(mHashArray);
+    mHashArray = nullptr;
+  }
+
+  if (mBuf) {
+    free(mBuf);
+    mBuf = nullptr;
+  }
+}
+
+nsresult
+CacheFileMetadata::ReadMetadata(CacheFileMetadataListener *aListener)
+{
+  MOZ_ASSERT(!mListener);
+  MOZ_ASSERT(!mHashArray);
+  MOZ_ASSERT(!mBuf);
+
+  nsresult rv;
+
+  int64_t size = mHandle->FileSize();
+  MOZ_ASSERT(size != -1);
+
+  if (size == 0) {
+    // this is a new entry
+    mOffset = 0;
+    aListener->OnMetadataRead(NS_OK);
+    mMetaHdr.mFetchCount++;
+    return NS_OK;
+  }
+
+  if (size < sizeof(CacheFileMetadataHeader) + 2*sizeof(uint32_t)) {
+    // there must be at least checksum, header and offset
+    aListener->OnMetadataRead(NS_ERROR_FILE_CORRUPTED);
+    return NS_OK;
+  }
+
+  // round offset to 4k blocks
+  int64_t offset = size & kAlignSize;
+
+  if (size - offset < kMinMetadataRead && offset > kAlignSize)
+    offset -= kAlignSize;
+
+  mBufSize = size - offset;
+  mBuf = static_cast<char *>(moz_xmalloc(mBufSize));
+
+  rv = CacheFileIOManager::Read(mHandle, offset, mBuf, mBufSize, this);
+  if (NS_FAILED(rv)) {
+    free(mWriteBuf);
+    mWriteBuf = nullptr;
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
+
+  mListener = aListener;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::WriteMetadata(uint32_t aOffset,
+                                 CacheFileMetadataListener *aListener)
+{
+  MOZ_ASSERT(!mListener);
+  MOZ_ASSERT(!mWriteBuf);
+
+  nsresult rv;
+
+  mWriteBuf = static_cast<char *>(moz_xmalloc(sizeof(uint32_t) +
+                mHashArraySize + sizeof(CacheFileMetadataHeader) +
+                mKey.Length() + 1 + mElementsSize + sizeof(uint32_t)));
+
+  char *p = mWriteBuf + sizeof(uint32_t);
+  memcpy(p, mHashArray, mHashArraySize);
+  p += mHashArraySize;
+  memcpy(p, &mMetaHdr, sizeof(CacheFileMetadataHeader));
+  p += sizeof(CacheFileMetadataHeader);
+  memcpy(p, mKey.get(), mKey.Length());
+  p += mKey.Length();
+  *p = 0;
+  p++;
+  memcpy(p, mBuf, mElementsSize);
+  p += sizeof(mElementsSize);
+
+  CacheHashUtils::Hash32_t hash;
+  hash = CacheHashUtils::Hash(mBuf + sizeof(uint32_t),
+                              p - mBuf - sizeof(uint32_t));
+  *reinterpret_cast<uint32_t *>(mWriteBuf) = htonl(hash);
+
+  *reinterpret_cast<uint32_t *>(p) = aOffset;
+  p += sizeof(uint32_t);
+
+  mListener = aListener;
+  rv = CacheFileIOManager::Write(mHandle, aOffset, mWriteBuf, p - mWriteBuf,
+                                 this);
+  if (NS_FAILED(rv)) {
+    mListener = nullptr;
+    free(mWriteBuf);
+    mWriteBuf = nullptr;
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
+
+  return NS_OK;
+}
+
+const char *
+CacheFileMetadata::GetElement(const char *aKey)
+{
+  const char *data = mBuf;
+  const char *limit = mBuf + mElementsSize;
+
+  while (data < limit) {
+    // Point to the value part
+    const char *value = data + strlen(data) + 1;
+    MOZ_ASSERT(value < limit, "Metadata elements corrupted");
+    if (strcmp(data, aKey) == 0)
+      return value;
+
+    // Skip value part
+    data = value + strlen(value) + 1;
+  }
+  MOZ_ASSERT(data == limit, "Metadata elements corrupted");
+  return nullptr;
+}
+
+nsresult
+CacheFileMetadata::SetElement(const char *aKey, const char *aValue)
+{
+  const uint32_t keySize = strlen(aKey) + 1;
+  char *pos = const_cast<char *>(GetElement(aKey));
+
+  if (!aValue) {
+    // No value means remove the key/value pair completely, if existing
+    if (pos) {
+      uint32_t oldValueSize = strlen(pos) + 1;
+      uint32_t offset = pos - mBuf;
+      uint32_t remainder = mElementsSize - (offset + oldValueSize);
+
+      memmove(pos - keySize, pos + oldValueSize, remainder);
+      mElementsSize -= keySize + oldValueSize;
+    }
+    return NS_OK;
+  }
+
+  const uint32_t valueSize = strlen(aValue) + 1;
+  uint32_t newSize = mElementsSize + valueSize;
+  if (pos) {
+    const uint32_t oldValueSize = strlen(pos) + 1;
+    const uint32_t offset = pos - mBuf;
+    const uint32_t remainder = mElementsSize - (offset + oldValueSize);
+
+    // Update the value in place
+    newSize -= oldValueSize;
+    EnsureBuffer(newSize);
+
+    // Move the remainder to the right place
+    pos = mBuf + offset;
+    memmove(pos + valueSize, pos + oldValueSize, remainder);
+  } else {
+    // allocate new meta data element
+    newSize += keySize;
+    EnsureBuffer(newSize);
+
+    // Add after last element
+    pos = mBuf + mElementsSize;
+    memcpy(pos, aKey, keySize);
+    pos += keySize;
+  }
+
+  // Update value
+  memcpy(pos, aValue, valueSize);
+  mElementsSize = newSize;
+
+  return NS_OK;
+}
+
+CacheHashUtils::Hash16_t
+CacheFileMetadata::GetHash(uint32_t aIndex)
+{
+  MOZ_ASSERT(aIndex < mHashCount);
+  return ntohs(mHashArray[aIndex]);
+}
+
+nsresult
+CacheFileMetadata::SetHash(uint32_t aIndex, CacheHashUtils::Hash16_t aHash)
+{
+  MOZ_ASSERT(aIndex <= mHashCount);
+
+  if (aIndex > mHashCount) {
+    return NS_ERROR_INVALID_ARG;
+  } else if (aIndex == mHashCount) {
+    if ((aIndex + 1) * sizeof(CacheHashUtils::Hash16_t) > mHashArraySize) {
+      // reallocate hash array buffer
+      if (mHashArraySize == 0)
+        mHashArraySize = 32 * sizeof(CacheHashUtils::Hash16_t);
+      else
+        mHashArraySize *= 2;
+      mHashArray = static_cast<CacheHashUtils::Hash16_t *>(
+                     moz_xrealloc(mHashArray, mHashArraySize));
+      mHashCount++;
+    }
+  }
+
+  mHashArray[aIndex] = htons(aHash);
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::SetExpirationTime(uint32_t aExpirationTime)
+{
+  mMetaHdr.mExpirationTime = aExpirationTime;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::GetExpirationTime(uint32_t *_retval)
+{
+  *_retval = mMetaHdr.mExpirationTime;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::SetLastModified(uint32_t aLastModified)
+{
+  mMetaHdr.mLastModified = aLastModified;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::GetLastModified(uint32_t *_retval)
+{
+  *_retval = mMetaHdr.mLastModified;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::GetLastFetched(uint32_t *_retval)
+{
+  *_retval = mMetaHdr.mLastFetched;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::GetFetchCount(uint32_t *_retval)
+{
+  *_retval = mMetaHdr.mFetchCount;
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::OnFileOpened(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_NOT_REACHED("CacheFileMetadata::OnFileOpened should not be called!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+nsresult
+CacheFileMetadata::OnDataWritten(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_ASSERT(mListener);
+  MOZ_ASSERT(mWriteBuf);
+
+  free(mWriteBuf);
+  mWriteBuf = nullptr;
+
+  nsCOMPtr<CacheFileMetadataListener> listener;
+
+  mListener.swap(listener);
+  listener->OnMetadataWritten(aResult);
+
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::OnDataRead(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_ASSERT(mListener);
+
+  nsresult rv;
+  nsCOMPtr<CacheFileMetadataListener> listener;
+
+  if (NS_FAILED(aResult)) {
+    mListener.swap(listener);
+    listener->OnMetadataRead(aResult);
+    return NS_OK;
+  }
+
+  // check whether we have read all necessary data
+  uint32_t realOffset = ntohl(*(reinterpret_cast<uint32_t *>(
+                                mBuf + mBufSize - sizeof(uint32_t))));
+
+  int64_t size = mHandle->FileSize();
+  MOZ_ASSERT(size != -1);
+
+  if (realOffset >= size) {
+    mListener.swap(listener);
+    listener->OnMetadataRead(NS_ERROR_FILE_CORRUPTED);
+    return NS_OK;
+  }
+
+  uint32_t usedOffset = size - mBufSize;
+
+  if (realOffset < usedOffset) {
+    uint32_t missing = usedOffset - realOffset;
+    // we need to read more data
+    mBuf = static_cast<char *>(moz_xrealloc(mBuf, mBufSize + missing));
+    memmove(mBuf + missing, mBuf, mBufSize);
+    mBufSize += missing;
+
+    rv = CacheFileIOManager::Read(mHandle, realOffset, mBuf, missing, this);
+    if (NS_FAILED(rv)) {
+      mListener.swap(listener);
+      listener->OnMetadataRead(rv);
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+
+    return NS_OK;
+  }
+
+  // We have all data according to offset information at the end of the entry.
+  // Try to parse it.
+  rv = ParseMetadata(realOffset, realOffset - usedOffset);
+
+  mListener.swap(listener);
+  listener->OnMetadataRead(rv);
+
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::OnFileDoomed(CacheFileHandle *aHandle, nsresult aResult)
+{
+  MOZ_NOT_REACHED("CacheFileMetadata::OnFileDoomed should not be called!");
+  return NS_ERROR_UNEXPECTED;
+}
+
+nsresult
+CacheFileMetadata::ParseMetadata(uint32_t aMetaOffset, uint32_t aBufOffset)
+{
+  nsresult rv;
+
+  uint32_t metaposOffset = mBufSize - sizeof(uint32_t);
+  uint32_t hashesOffset = aBufOffset + sizeof(uint32_t);
+  uint32_t hashCount = aMetaOffset / kChunkSize;
+  if (aMetaOffset % kChunkSize)
+    hashCount++;
+  uint32_t hashesLen = hashCount * sizeof(CacheHashUtils::Hash16_t);
+  uint32_t hdrOffset = hashesOffset + hashesLen;
+  uint32_t keyOffset = hdrOffset + sizeof(CacheFileMetadataHeader);
+
+  if (keyOffset > metaposOffset)
+    return NS_ERROR_FILE_CORRUPTED;
+
+  uint32_t elementsOffset = reinterpret_cast<CacheFileMetadataHeader *>(
+                              mBuf + hdrOffset)->mKeySize + keyOffset;
+
+  if (elementsOffset > metaposOffset)
+    return NS_ERROR_FILE_CORRUPTED;
+
+  // check that key ends with \0
+  if (mBuf[elementsOffset - 1] != 0)
+    return NS_ERROR_FILE_CORRUPTED;
+
+  if (reinterpret_cast<CacheFileMetadataHeader *>(mBuf + hdrOffset)->mKeySize !=
+      mKey.Length())
+    return NS_ERROR_FILE_CORRUPTED;
+
+  if (memcmp(mKey.get(), mBuf + keyOffset, mKey.Length()) != 0)
+    return NS_ERROR_FILE_CORRUPTED;
+
+  // check metadata hash (data from hashesOffset to metaposOffset)
+  CacheHashUtils::Hash32_t hash;
+  hash = CacheHashUtils::Hash(mBuf + hashesOffset,
+                              metaposOffset - hashesOffset);
+
+  if (hash != ntohl(*(reinterpret_cast<uint32_t *>(mBuf + aBufOffset))))
+    return NS_ERROR_FILE_CORRUPTED;
+
+  // check elements
+  rv = CheckElements(mBuf + elementsOffset, metaposOffset - elementsOffset);
+  if (NS_FAILED(rv))
+    return rv;
+
+  mHashArraySize = hashesLen;
+  mHashCount = hashCount;
+  if (mHashArraySize) {
+    mHashArray = static_cast<CacheHashUtils::Hash16_t *>(
+                   moz_xmalloc(mHashArraySize));
+    memcpy(mHashArray, mBuf + hashesOffset, mHashArraySize);
+  }
+
+  memcpy(&mMetaHdr, mBuf + hdrOffset, sizeof(CacheFileMetadataHeader));
+  mMetaHdr.mFetchCount++;
+
+  mElementsSize = metaposOffset - elementsOffset;
+  memmove(mBuf, mBuf + elementsOffset, mElementsSize);
+  mOffset = aMetaOffset;
+
+  // TODO: shrink memory if buffer is too big
+
+  return NS_OK;
+}
+
+nsresult
+CacheFileMetadata::CheckElements(const char *aBuf, uint32_t aSize)
+{
+  if (aSize) {
+    // Check if the metadata ends with a zero byte.
+    if (aBuf[aSize - 1] != 0) {
+      NS_ERROR("Metadata elements are not null terminated");
+      return NS_ERROR_FILE_CORRUPTED;
+    }
+    // Check that there are an even number of zero bytes
+    // to match the pattern { key \0 value \0 }
+    bool odd = false;
+    for (uint32_t i = 0; i < aSize; i++) {
+      if (aBuf[i] == 0)
+        odd = !odd;
+    }
+    if (odd) {
+      NS_ERROR("Metadata elements are malformed");
+      return NS_ERROR_FILE_CORRUPTED;
+    }
+  }
+  return NS_OK;
+}
+
+void
+CacheFileMetadata::EnsureBuffer(uint32_t aSize)
+{
+  if (mBufSize < aSize) {
+    mBufSize = aSize;
+    mBuf = static_cast<char *>(moz_xrealloc(mBuf, mBufSize));
+  }
+}
+
+} // net
+} // mozilla
