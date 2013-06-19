dccdcb502c303d38eaec5f924654dde2e992bbb9
Honza Bambas <honzab.moz@firemni.cz>
2013-06-19 21:02:09 +0200

    CACHE2: basic xpcshell test split to individual tests

---
+++ netwerk/test/unit/test_cache2-18-not-valid.js
@@ -0,0 +1,23 @@
+function run_test()
+{
+  do_get_profile();
+
+  // Open for write, write
+  asyncOpenCacheEntry("http://v/", "disk", Ci.nsICacheStorage.OPEN_NORMALLY, null,
+    new OpenCallback(NEW, "v1m", "v1d", function(entry) {
+      // Open for rewrite (don't validate), write different meta and data
+      asyncOpenCacheEntry("http://v/", "disk", Ci.nsICacheStorage.OPEN_NORMALLY, null,
+        new OpenCallback(NOTVALID|NEW, "v2m", "v2d", function(entry) {
+          // And check...
+          asyncOpenCacheEntry("http://v/", "disk", Ci.nsICacheStorage.OPEN_NORMALLY, null,
+            new OpenCallback(NORMAL, "v2m", "v2d", function(entry) {
+              finish_cache2_test();
+            })
+          );
+        })
+      );
+    })
+  );
+
+  do_test_pending();
+}
