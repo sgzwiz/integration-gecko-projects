88f4c229096a96fdc68098f9ec25160337a9d6b6
Honza Bambas <honzab.moz@firemni.cz>
2013-07-03 12:48:03 +0200

    CACHE2: Fix intermittent gzip 206 test failure: root helper cache listener in the target listener

--- netwerk/test/unit/test_cache2-19-range-206.js
+++ netwerk/test/unit/test_cache2-19-range-206.js
@@ -10,7 +10,7 @@ function run_test()
         new OpenCallback(PARTIAL, "206m", "206part1-", function(entry) {
           // emulate 206 from the server, i.e. resume transaction and write content to the output stream
           (new OpenCallback(NEW|WAITFORWRITE|PARTIAL, "206m", "-part2", function(entry) {
-            entry.setValid(); // I think this is not needed... opening output stream validates the entry
+            entry.setValid();
           })).onCacheEntryAvailable(entry, true, null, Cr.NS_OK);
         })
       );
