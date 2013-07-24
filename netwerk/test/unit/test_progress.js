a19c19e565ab75e854bcb9ecfc1a31eb9ddc7ba7
Honza Bambas <honzab.moz@firemni.cz>
2013-07-24 23:54:17 +0200

    CACHE2 MERGE: Re-apply conflicting file changes

--- netwerk/test/unit/test_progress.js
+++ netwerk/test/unit/test_progress.js
@@ -1,5 +1,9 @@
 Cu.import("resource://testing-common/httpd.js");
 
+XPCOMUtils.defineLazyGetter(this, "URL", function() {
+  return "http://localhost:" + httpserver.identity.primaryPort;
+});
+
 var httpserver = new HttpServer();
 var testpath = "/simple";
 var httpbody = "0123456789";
@@ -93,7 +97,7 @@ var progressCallback = {
 
 function run_test() {
   httpserver.registerPathHandler(testpath, serverHandler);
-  httpserver.start(4444);
+  httpserver.start(-1);
   var channel = setupChannel(testpath);
   channel.asyncOpen(progressCallback, null);
   do_test_pending();
@@ -102,7 +106,7 @@ function run_test() {
 function setupChannel(path) {
   var ios = Cc["@mozilla.org/network/io-service;1"].
                        getService(Ci.nsIIOService);
-  var chan = ios.newChannel("http://localhost:4444" + path, "", null);
+  var chan = ios.newChannel(URL + path, "", null);
   chan.QueryInterface(Ci.nsIHttpChannel);
   chan.requestMethod = "GET";
   chan.notificationCallbacks = progressCallback;
