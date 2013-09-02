416c9796535d96c7e4d85d84e2b93005e0ba552d
Honza Bambas <honzab.moz@firemni.cz>
2013-09-02 17:24:53 +0200

    CACHE2: old cache - syncWithCacheIOThread before visit in 05,06,07 cache2 tests

--- netwerk/test/unit/test_cache2-07-visit-memory.js
+++ netwerk/test/unit/test_cache2-07-visit-memory.js
@@ -10,18 +10,22 @@ function run_test()
   // Add entry to the memory storage
   var mc = new MultipleCallbacks(5, function() {
     // Check it's there by visiting the storage
-    var storage = getCacheStorage("memory");
-    storage.asyncVisitStorage(
-      new VisitCallback(1, 12, ["http://mem1/"], function() {
-        storage = getCacheStorage("disk");
-        storage.asyncVisitStorage(
-          // Previous tests should store 4 disk entries + 1 memory entry
-          new VisitCallback(5, 60, ["http://a/", "http://b/", "http://c/", "http://d/", "http://mem1/"], function() {
-            finish_cache2_test();
-          }),
-        true);
-      }),
-    true);
+    syncWithCacheIOThread(function() {
+      var storage = getCacheStorage("memory");
+      storage.asyncVisitStorage(
+        new VisitCallback(1, 12, ["http://mem1/"], function() {
+          storage = getCacheStorage("disk");
+          storage.asyncVisitStorage(
+            // Previous tests should store 4 disk entries + 1 memory entry
+            new VisitCallback(5, 60, ["http://a/", "http://b/", "http://c/", "http://d/", "http://mem1/"], function() {
+              finish_cache2_test();
+            }),
+            true
+          );
+        }),
+        true
+      );
+    });
   });
 
   asyncOpenCacheEntry("http://mem1/", "memory", Ci.nsICacheStorage.OPEN_NORMALLY, null,
