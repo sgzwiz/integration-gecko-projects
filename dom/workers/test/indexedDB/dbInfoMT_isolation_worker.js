85c4c2a02f4d22bf4035a9a25a30ca4cc75c212b
Vendelin Ruzicka <vendo.ruzicka@gmail.com>
2013-09-11 08:31:04 +0200

    IDBSYNC: Bug 798875 - New test for dbInfoMT_isolation.

---
+++ dom/workers/test/indexedDB/dbInfoMT_isolation_worker.js
@@ -0,0 +1,29 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+importScripts("helpers_worker.js");
+
+onmessage = function(event) {
+  var data = event.data;
+  var objectStoreData = data.objectStoreData;
+
+  var db = indexedDBSync.open(data.name, data.version, function(trans, oldVersion) {
+    ok(false, "Unexpected upgradeneeded callback ");
+  });
+
+  db.transaction(data.objectStoreName, function(trans) {
+    var store = trans.objectStore(data.objectStoreName);
+
+    ok(store.get(objectStoreData.key).name == objectStoreData.value.name,
+       "Correct data");
+
+    var index = store.index(data.indexData.name);
+    ok(index.get(objectStoreData.value.name).name == objectStoreData.value.name,
+       "Correct data");
+
+  });
+
+  postMessage(undefined);
+};
