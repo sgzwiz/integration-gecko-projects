012a7e69033f8df6a9371aff0da6e261cc400907
Blake Kaplan <mrbkap@gmail.com>
2013-10-28 11:19:46 -0700

    Bug 924702 - Fix an XPCOM violation, compartment mismatch and rooting error.

--- netwerk/protocol/app/AppProtocolHandler.cpp
+++ netwerk/protocol/app/AppProtocolHandler.cpp
@@ -341,10 +341,7 @@ NS_IMETHODIMP
 AppProtocolHandler::NewChannel(nsIURI* aUri, nsIChannel* *aResult)
 {
   NS_ENSURE_ARG_POINTER(aUri);
-  nsJARChannel* channel = new nsJARChannel();
-  if (!channel) {
-    return NS_ERROR_OUT_OF_MEMORY;
-  }
+  nsRefPtr<nsJARChannel> channel = new nsJARChannel();
 
   nsAutoCString host;
   nsresult rv = aUri->GetHost(host);
@@ -363,26 +360,24 @@ AppProtocolHandler::NewChannel(nsIURI* aUri, nsIChannel* *aResult)
       return NS_ERROR_FAILURE;
     }
 
-    JS::Value jsInfo;
-    rv = appsService->GetAppInfo(NS_ConvertUTF8toUTF16(host), &jsInfo);
-    if (NS_FAILED(rv)) {
-      // Return a DummyChannel.
-      delete channel;
-      NS_IF_ADDREF(*aResult = new DummyChannel());
-      return NS_OK;
-    }
-
     mozilla::AutoSafeJSContext cx;
-    appInfo = new mozilla::dom::AppInfo();
-    if (!appInfo->Init(cx, JS::Handle<JS::Value>::fromMarkedLocation(&jsInfo)) ||
-        appInfo->mPath.IsEmpty()) {
-      printf_stderr("!! No appInfo for %s\n", host.get());
+    JS::RootedValue jsInfo(cx);
+    rv = appsService->GetAppInfo(NS_ConvertUTF8toUTF16(host), jsInfo.address());
+    if (NS_FAILED(rv) || !jsInfo.isObject()) {
       // Return a DummyChannel.
-      delete channel;
+      printf_stderr("!! Creating a dummy channel for %s (no appInfo)\n", host.get());
       NS_IF_ADDREF(*aResult = new DummyChannel());
       return NS_OK;
     }
 
+    appInfo = new mozilla::dom::AppInfo();
+    JSAutoCompartment ac(cx, &jsInfo.toObject());
+    if (!appInfo->Init(cx, jsInfo) || appInfo->mPath.IsEmpty()) {
+      // Return a DummyChannel.
+      printf_stderr("!! Creating a dummy channel for %s (invalid appInfo)\n", host.get());
+      NS_IF_ADDREF(*aResult = new DummyChannel());
+      return NS_OK;
+    }
     mAppInfoCache.Put(host, appInfo);
   }
 
@@ -410,7 +405,7 @@ AppProtocolHandler::NewChannel(nsIURI* aUri, nsIChannel* *aResult)
   rv = channel->SetOriginalURI(aUri);
   NS_ENSURE_SUCCESS(rv, rv);
 
-  NS_ADDREF(*aResult = channel);
+  channel.forget(aResult);
   return NS_OK;
 }
 
