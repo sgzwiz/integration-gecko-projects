e95f185de42906c774e94d7c91cb144ed3d2ead0
Honza Bambas <honzab.moz@firemni.cz>
2013-11-08 18:09:45 +0100

    Bug 913806 - turn HTTP cache v2 on by default

--- netwerk/cache2/CacheObserver.cpp
+++ netwerk/cache2/CacheObserver.cpp
@@ -19,7 +19,7 @@ CacheObserver* CacheObserver::sSelf = nullptr;
 static uint32_t const kDefaultMemoryLimit = 50 * 1024; // 50 MB
 uint32_t CacheObserver::sMemoryLimit = kDefaultMemoryLimit;
 
-static uint32_t const kDefaultUseNewCache = 0; // Don't use the new cache by default
+static uint32_t const kDefaultUseNewCache = 1; // Use the new cache by default
 uint32_t CacheObserver::sUseNewCache = kDefaultUseNewCache;
 
 NS_IMPL_ISUPPORTS2(CacheObserver,
