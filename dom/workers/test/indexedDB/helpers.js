d3de35cfce7fc7836af0f8d9bdab5da51b33c1f5
Jan Varga <jan.varga@gmail.com>
2013-11-09 08:22:00 +0100

    SYNCIDB: Android and B2G fixes.

--- dom/workers/test/indexedDB/helpers.js
+++ dom/workers/test/indexedDB/helpers.js
@@ -3,9 +3,29 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
+Components = SpecialPowers.Components;
+
+function clearAllDatabases() {
+  if (!SpecialPowers.isMainProcess()) {
+    return;
+  }
+
+  let comp = SpecialPowers.wrap(Components);
+
+  let quotaManager =
+    comp.classes["@mozilla.org/dom/quota/manager;1"]
+        .getService(comp.interfaces.nsIQuotaManager);
+
+  let uri = SpecialPowers.wrap(document).documentURIObject;
+
+  quotaManager.clearStoragesForURI(uri);
+}
+
 if (!window.runTest) {
   window.runTest = function(path) {
     const indexedDBSyncPref = "dom.indexedDBSync.enabled";
+
+    clearAllDatabases();
 
     SpecialPowers.pushPrefEnv({ set: [[indexedDBSyncPref, true]] }, function() {
       var worker = new Worker(path);
