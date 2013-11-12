78801675faca3a7d9dc77062c1e48c2e96983a51
Peter Van der Beken <peterv@propagandism.org>
2013-11-12 12:14:17 -0800

    Bug 935786 - Assertion failure: !IsInnerWindow() || IsCurrentInnerWindow() at nsGlobalWindow.cpp:8909.

--- embedding/components/windowwatcher/src/nsAutoWindowStateHelper.cpp
+++ embedding/components/windowwatcher/src/nsAutoWindowStateHelper.cpp
@@ -47,7 +47,7 @@ bool
 nsAutoWindowStateHelper::DispatchEventToChrome(const char *aEventName)
 {
   nsCOMPtr<nsPIDOMWindow> window = do_QueryInterface(mWindow);
-  if (!window) {
+  if (!window || !window->IsCurrentInnerWindow()) {
     return true;
   }
 
